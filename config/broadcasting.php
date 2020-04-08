65c182aefd37221e17083e88ba4cbb220d706640
Kevin Namuag <kevsnams@pm.me>
2020-04-08 12:08:05 +0800

    fixed websockets for SSL enabled sites

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'encrypted' => true,
+                'encrypted' => env('APP_IS_SSL', false),
                 'host' => env('PUSHER_OPTIONS_HOST'),
                 'port' => 6001,
                 'scheme' => 'http'
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: true,
+    encrypted: process.env.APP_IS_SSL,
     wsHost: window.location.hostname,
-    wsPort: 6001
+    wsPort: 6001,
+    disableStats: true
 });
