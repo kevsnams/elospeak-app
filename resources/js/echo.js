53ae6e07f1bc52f84c125cf12ef90fe0af43a345
Kevin Namuag <kevsnams@pm.me>
2020-04-08 14:28:55 +0800

    added back encrypted'

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,6 +7,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    encrypted: process.env.APP_IS_SSL,
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true
