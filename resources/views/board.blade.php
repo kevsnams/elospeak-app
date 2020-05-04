d1b5c9af2c47f5ecbbb762a539b74cce92cde34b
Kevin Namuag <kevsnams@pm.me>
2020-05-04 19:39:07 +0800

    force desktop view for mobile

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <meta name="viewport" content="width=1024">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>Board</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Patrick+Hand|Quicksand:300,400,700&display=swap">
