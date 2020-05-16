c85506f1342ea56cac0909da1c096a690d004252
Kevin Namuag <kevsnams@pm.me>
2020-05-16 11:50:00 +0800

    Code cleanup - removed old blade files

--- resources/views/includes/sideNav.blade.php
+++
@@ -1,6 +0,0 @@
-<nav {{ isset($id) ? 'id="'. $id .'"' : '' }} class="{{ $class }}">
-    {{--- $item will contain ['route' => '', 'title' => '', 'icon' => ''] ---}}
-    @foreach ($items as $item)
-        @include('includes.sideNavItem', $item)
-    @endforeach
-</nav>
--- resources/views/includes/sideNavItem.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ $route }}" class="@yield('vNav-active-'. $active, '')">
-    <span uk-icon="icon: {{ $icon }}; ratio: 2"></span>
-    {{ $text }}
-</a>
