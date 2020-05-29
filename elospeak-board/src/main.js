89a78dd1974b7e88e7e5664114f7d03801e5212e
Kevin Namuag <kevsnams@pm.me>
2020-05-29 23:26:47 +0800

    Added CSRF token for axios globally

--- elospeak-board/src/main.js
+++ elospeak-board/src/main.js
@@ -1,6 +1,14 @@
 import 'bootstrap';
 import jstz from 'jstimezonedetect';
 import App from './App.svelte';
+import axios from 'axios';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 const app = new App({
 	target: document.getElementById('main'),
